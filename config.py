9fea29e98ab6b8f34247f489fdb1d9fc5cfb8be6
tylernchls <tylernchls@gmail.com>
2018-02-27 17:41:56 -1000

    updates .gitignore

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# references DB object in __init__.py
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    #Flask-WTF extension uses it secret key to protect web forms against CSRS attacks
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'xxxxx BAHAHA'
+
+    # Uses env variable for DB path, sets tracking to false so doesnt single app everytime change is going to happen to DB
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
